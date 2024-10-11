fdf4ce0164e2d445b0fa641391c1a3c5223ea963
smallgod <davies.mugume@gmail.com>
2024-10-11 17:36:32 +0300

    added PK to obs groups flat tables

--- api/src/main/resources/_core/database/mysql/xf_system/etl_flat_table_obs_group/sp_mamba_flat_encounter_obs_group_table_create.sql
+++ api/src/main/resources/_core/database/mysql/xf_system/etl_flat_table_obs_group/sp_mamba_flat_encounter_obs_group_table_create.sql
@@ -31,7 +31,7 @@ BEGIN
     IF @column_labels IS NOT NULL THEN
             SET @create_table = CONCAT(
                     'CREATE TABLE `', @tbl_obs_group_name,
-                    '` (`encounter_id` INT NOT NULL, `visit_id` INT NULL, `client_id` INT NOT NULL, `encounter_datetime` DATETIME NOT NULL, `location_id` INT NULL, ',
+                    '` (`encounter_id` INT NOT NULL PRIMARY KEY, `visit_id` INT NULL, `client_id` INT NOT NULL, `encounter_datetime` DATETIME NOT NULL, `location_id` INT NULL, ',
                     @column_labels,
                     ', INDEX `mamba_idx_encounter_id` (`encounter_id`), INDEX `mamba_idx_visit_id` (`visit_id`), INDEX `mamba_idx_client_id` (`client_id`), INDEX `mamba_idx_encounter_datetime` (`encounter_datetime`), INDEX `mamba_idx_location_id` (`location_id`));');
     END IF;
